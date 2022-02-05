5aef5ede8e99c527ab093ef5ee7475d0662b23a7
SergeyGotenko <gotenkosergey@gmail.com>
2022-02-05 22:34:39 +0200

    fix -2 task_6

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,33 @@
+
+
 const input = document.querySelector('#validation-input');
+
 
 input.addEventListener('blur', onValidationInput);
 
 
-function onValidationInput(event) {
-    if (Number(event.currentTarget.dataset.length) === event.currentTarget.value.length) {
-        event.currentTarget.classList.add('valid')
-        event.currentTarget.classList.remove('invalid')
-    } else {
-         event.currentTarget.classList.remove('valid')
-        event.currentTarget.classList.add('invalid')
-    }
-    // console.log(event.currentTarget.value.length)
-    // console.log(event.currentTarget.dataset.length)
-}
+// function onValidationInput(event) {
+//     if (Number(event.currentTarget.dataset.length) === event.currentTarget.value.length) {
+//         event.currentTarget.classList.add('valid')
+//         event.currentTarget.classList.remove('invalid')
+//     } else {
+//          event.currentTarget.classList.remove('valid')
+//         event.currentTarget.classList.add('invalid')
+//     }
+//     // console.log(event.currentTarget.value.length)
+//     // console.log(event.currentTarget.dataset.length)
+// }
 
+
+
+function onValidationInput(event) {
+
+    input.classList.add('valid', 'invalid');
+
+    const defaultValue = Number(event.currentTarget.dataset.length);
+    const inputValue = event.currentTarget.value.length;
+
+    if (defaultValue === inputValue) {
+        input.classList.toggle('invalid');
+     }
+}
