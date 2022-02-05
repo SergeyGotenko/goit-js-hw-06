27d934f3e97baaf10855ddb0fa2e3de49127d520
SergeyGotenko <gotenkosergey@gmail.com>
2022-02-05 20:27:16 +0200

    fix task_6

--- js/task-06.js
+++ js/task-06.js
@@ -4,12 +4,14 @@ input.addEventListener('blur', onValidationInput);
 
 
 function onValidationInput(event) {
-    if (event.currentTarget.dataset.length == event.currentTarget.value.length) {
+    if (Number(event.currentTarget.dataset.length) === event.currentTarget.value.length) {
         event.currentTarget.classList.add('valid')
         event.currentTarget.classList.remove('invalid')
     } else {
          event.currentTarget.classList.remove('valid')
         event.currentTarget.classList.add('invalid')
     }
+    // console.log(event.currentTarget.value.length)
+    // console.log(event.currentTarget.dataset.length)
 }
 
